355ae1a945fa09f1a1b1458229d4a38cee317c31
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-13 13:55:17 -0600

    pushing files before pull request

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const getMovies = require('./api.js');
+const movieDisplay = document.getElementByID("movieDisplay");
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+    movieDisplay.innerHTML = `id#${id} - ${title} - rating: ${rating}`;
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
