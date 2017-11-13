98488c59f27801c3f3858a7b0c9c1f8588f629df
Tito Valiente <titolvaliente@gmail.com>
2017-11-13 16:43:03 -0600

    finished page load

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
+const $ = require("jquery");
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
+    const $loading = $('#loading').hide();
+    $(document)
+        .ajaxStart(function () {
+            $loading.hide('body');
+        })
+        .ajaxStop(function () {
+            $loading.show('body');
+        });
+
   console.log('Here are all the movies:');
+  let movieTitles = "";
   movies.forEach(({title, rating, id}) => {
+   movieTitles +=  `<div> ${title} - rating: ${rating} </div>`;
+       $('#movies').html(movieTitles);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
