96fdd37082f5449fbbe5bf5f51a27e390ad19a22
Tito Valiente <titolvaliente@gmail.com>
2017-11-14 16:02:29 -0600

    finished adding movie feature

--- src/index.js
+++ src/index.js
@@ -10,25 +10,54 @@
 const $ = require("jquery");
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-    const $loading = $('#loading').hide();
-    $(document)
-        .ajaxStart(function () {
-            $loading.hide('body');
-        })
-        .ajaxStop(function () {
-            $loading.show('body');
-        });
+//loading function
+ function updateMovies() {
+     getMovies().then((movies) => {
+         const $loading = $('#loading').hide();
+         $(document)
+             .ajaxStart(function () {
+                 $loading.hide('body');
+             })
+             .ajaxStop(function () {
+                 $loading.show('body');
+             });
 
-  console.log('Here are all the movies:');
-  let movieTitles = "";
-  movies.forEach(({title, rating, id}) => {
-   movieTitles +=  `<div> ${title} - rating: ${rating} </div>`;
-       $('#movies').html(movieTitles);
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+         console.log('Here are all the movies:');
+         let movieTitles = "";
+         movies.forEach(({title, rating, id}) => {
+             movieTitles += `<div><p>${title} - rating: ${rating}</p></div>`;
+             $('#movies').html(movieTitles);
+             console.log(`id#${id} - ${title} - rating: ${rating}`);
+         });
+     }).catch((error) => {
+         alert('Oh no! Something went wrong.\nCheck the console for details.');
+         console.log(error);
+     });
+ }
+ updateMovies();
+//New movie submit
+$("#newMovie").click(function(e){
+
+    e.preventDefault();
+    let title = $('#movieTitle').val();
+    let rating = $('input[name=optradio]:checked').val();
+    let formData = {title:title,rating:rating}; //Array
+    $('#addMovie').fadeOut(800);
+    $.ajax({
+        url : "/api/movies",
+        type: "POST",
+        data : formData,
+        success: function() {
+
+            $('#addMovie').fadeIn(2000);
+
+            updateMovies();
+            $('#movieTitle').val('');
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Something is wrong');
+        }
+    });
 });
 
