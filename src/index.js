27d0ad5d176e413033d36a366e3483ee416070c8
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-15 17:03:06 -0600

    worked on html and css, added trash/edit buttons added functions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@
 const $ = require("jquery");
 const {getMovies} = require('./api.js');
 
+
 //loading function
 function updateMovies() {
     getMovies().then((movies) => {
@@ -25,15 +26,19 @@ function updateMovies() {
         console.log('Here are all the movies:');
         let movieTitles = "";
         movies.forEach(({title, rating, id}) => {
-            movieTitles += `<div><p>${title} - rating: ${rating}</p></div>`;
+            movieTitles +=
+                `<div><p><h4>${title}</h4>Rating: ${rating}<br><span class="glyphicon glyphicon-edit tooltiptext" id="edit${id}"></span>     <span class="glyphicon glyphicon-trash tooltiptext" id="trash${id}"></span></p></div>`;
             $('#movies').html(movieTitles);
             console.log(`id#${id} - ${title} - rating: ${rating}`);
         });
+        makingMoviesGarbage(movies);
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
         console.log(error);
     });
 }
+
+
 updateMovies();
 //New movie submit
 $("#newMovie").click(function(e){
@@ -44,7 +49,7 @@ $("#newMovie").click(function(e){
     let formData = {title:title,rating:rating}; //Array
     $('#addMovie').fadeOut(800);
     $.ajax({
-        url : "/api/movies",
+        url : "/api/movies/",
         type: "POST",
         data : formData,
         success: function() {
@@ -71,7 +76,7 @@ $("#editMovie").click(function(e) {
     let formData = {title:title,rating:rating};
     $('#editMovieBtn').fadeOut(800);
     $.ajax({
-        url : "/api/movies",
+        url : "/api/movies/",
         type: "PUT",
         data : formData,
         success: function() {
@@ -90,30 +95,29 @@ $("#editMovie").click(function(e) {
 });
 
 //Delete Movie
-$('#deleteMovie').click(function (e) {
+function makingMoviesGarbage(movies){
+movies.forEach(({title, rating, id}) => {
+    $(`#trash${id}`).click(function (e) {
+        // e.preventDefault();
+        // let title = $('#movieTitle').val();
+        // let rating = $('input[name=optradio]:checked').val();
+        // let formData = {title: title, rating: rating};
+        // $('#deleteMovie').fadeOut(800);
+        $.ajax({
+            url: `/api/movies/${id}`,
+            type: "DELETE",
+            success: function () {
 
-    e.preventDefault();
-    let title = $('#movieTitle').val();
-    let rating = $('input[name=optradio]:checked').val();
-    let formData = {title:title,rating:rating};
-    $('#deleteMovie').fadeOut(800);
-    $.ajax({
-        url : "/api/movies",
-        type: "DELETE",
-        data : formData,
-        success: function() {
+                $('#deleteMovie').fadeIn(2000);
 
-            $('#deleteMovie').fadeIn(2000);
+                updateMovies();
+                $('#movieTitle').val('');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('Something is wrong');
+            }
+        });
 
-            updateMovies();
-            $('#movieTitle').val('');
-        },
-        error: function (jqXHR, textStatus, errorThrown)
-        {
-            alert('Something is wrong');
-        }
     });
-
-
-
-});
+})
+}
